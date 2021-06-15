015ad509a63c08f29645272f6fa25a73f0fd63b4
umamaheshg-it <65701184+umamaheshg-it@users.noreply.github.com>
2021-06-15 10:31:06 +0530

    usage of state in components

---
+++ stateusage/src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Message from "./components/Message";
+import Welcome from "./components/Welcome";
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <Message />
+    </div>
+  );
+}
+
+export default App;
