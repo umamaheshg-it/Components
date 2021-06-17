ddd468196dad261b1307d30aae3c25b7bfd076a0
umamaheshg-it <65701184+umamaheshg-it@users.noreply.github.com>
2021-06-17 18:44:51 +0530

    State Hook

---
+++ usestatewithobjecthook/src/App.js
@@ -0,0 +1,12 @@
+import SetNameHook from "./components/SetNameHook";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>
+        <SetNameHook />
+      </h1>
+    </div>
+  );
+}
